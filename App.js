93376edb64a88b62206c334dffdf073a1d3ad9c6
SehajpreetKaurKhurana <78088098+SehajpreetKaurKhurana@users.noreply.github.com>
2021-11-13 21:11:01 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import DataEntry from './DataEntry';
+
+function App() {
+  return (
+    <div className="App">
+      <DataEntry/>
+        
+      
+    </div>
+  );
+}
+
+export default App;
